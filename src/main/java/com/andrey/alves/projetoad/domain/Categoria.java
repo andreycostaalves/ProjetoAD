bdcda92d1d0c5101db554e2890481067e90c47d5
andrey alves <andreyalvesgit@gmail.com>
2019-03-15 15:38:32 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/andrey/alves/projetoad/domain/Categoria.java
+++ src/main/java/com/andrey/alves/projetoad/domain/Categoria.java
@@ -2,10 +2,19 @@ package com.andrey.alves.projetoad.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//gera o valor do "id" automaticamente.
 	private Integer id;
 	private String nome;
 	

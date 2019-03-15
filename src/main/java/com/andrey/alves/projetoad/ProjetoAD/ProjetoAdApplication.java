49c622b8bdc156f8cce1ce9864c448e705f57131
andrey alves <andreyalvesgit@gmail.com>
2019-03-15 11:39:05 -0300

    Inicio de projeto

---
+++ src/main/java/com/andrey/alves/projetoad/ProjetoAD/ProjetoAdApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.alves.projetoad.ProjetoAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrey/alves/projetoad/ProjetoAD/ProjetoAdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.andrey.alves.projetoad.ProjetoAD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoAdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

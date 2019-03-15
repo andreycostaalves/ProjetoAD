3ec725485790b853ee0b1f044ee1a3465456c329
andrey alves <andreyalvesgit@gmail.com>
2019-03-15 14:14:26 -0300

    Rest TESTADO.

--- src/main/java/com/andrey/alves/projetoad/ProjetoAdApplication.java
+++ src/main/java/com/andrey/alves/projetoad/ProjetoAdApplication.java
@@ -1,4 +1,4 @@
-package com.andrey.alves.projetoad.ProjetoAD;
+package com.andrey.alves.projetoad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
